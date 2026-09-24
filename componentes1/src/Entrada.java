37d73c5b917c3073186706660cf2a74345f6d67e
alicia :) <aliciadragotadam@gmail.com>
2026-09-24 19:27:12 +0200

    update

--- componentes1/src/Entrada.java
+++ componentes1/src/Entrada.java
@@ -2,6 +2,11 @@ public class Entrada {
 
     public static void main (String[] args) {
         System.out.println("Hola mundo");
+        String nombreLegal = "Alicia";
+        int edad = 20;
+        System.out.println("Mi nombre es "+nombreLegal);
+        System.out.println("Tengo "+edad+" años");
+        char gato = 'g';
+        System.out.println("La palabra gato empieza con la letra "+gato);
     }
-
 }
